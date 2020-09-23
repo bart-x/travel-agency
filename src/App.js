dc3caa67864d8d48cf8f9b8ee24b32fa39dbdc31
Bartek K <kontener00@gmail.com>
2020-09-23 19:22:32 +0200

    Added routing to other (sub)pages in App component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
-// TODO - import other views
+// TODO - import other views -> DONE !!!
+import Trip from './components/views/Trip/TripContainer';
+import Country from './components/views/Country/CountryContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -40,7 +45,12 @@ class App extends React.Component {
           <Switch location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
+            {/* TODO - add more routes for other views -> DONE!!!*/}
+            <Route exact path="/trip/:id" component={Trip} />
+            <Route exact path="/countries" component={Countries} />
+            <Route exact path="/country/:id" component={Country} />
+            <Route exact path="/regions" component={Regions} />
+
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
