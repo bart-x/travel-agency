02ea3e777bd8ab34106dcd46b8c20dc76a1551dc
Bartek K <kontener00@gmail.com>
2020-09-27 23:15:23 +0200

    Added API data, order handling

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,35 +1,78 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
-import OrderSummary from '../OrderSummary/OrderSummary';
+import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
+
+import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings.js';
 
-class OrderForm extends React.Component {
+const sendOrder = (options, tripCost, tripName, tripId, tripCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
 
-  static propTypes = {
-    tripCost: PropTypes.node,
-    options: PropTypes.object,
-    setOrderOption: PropTypes.func,
+  const { contact, name } = options;
+
+  if (name === '' || name.length < 5) {
+    alert('Please fill name with 5 or more letters');
+    return;
+  }
+  if (contact === '' || contact.length < 5) {
+    alert('Please fill contact with 5 or more letters');
+    return;
   }
 
-  render() {
-    const { tripCost, options, setOrderOption } = this.props;
-    return (
-      <Row>
-        {pricing.map(option =>
-          <Col md={4} key={option.id}>
-            <OrderOption {...option} currentValue={option[option.id]}
-              setOrderOption={setOrderOption} />
-          </Col>
-        )}
-        <Col xs={12}>
-          <OrderSummary tripCost={tripCost} options={options} />
-        </Col>
-      </Row>
-    );
-  }
-}
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    tripId,
+    tripCode,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function (response) {
+      return response.json();
+    }).then(function (parsedResponse) {
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({ tripCost, options, setOrderOption, tripName, tripId, tripCode }) => (
+  <Row>
+    {pricing.map(option => (
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
+      </Col>
+    ))}
+    <Col xs={12}>
+      <OrderSummary tripCost={tripCost} options={options} />
+    </Col>
+    <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, tripCode)}>Order now!</Button>
+  </Row>
+);
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+  setOrderOption: PropTypes.func,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
+  tripCode: PropTypes.string,
+};
 
 export default OrderForm;
--- src/components/views/Trip/Trip.js
+++ src/components/views/Trip/Trip.js
@@ -1,35 +1,92 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HTMLParser from 'react-html-parser';
 
-import TripSummary from '../../features/TripSummary/TripSummary';
+import NotFound from '../NotFound/NotFound';
 import Section from '../../layout/Section/Section';
 import PageTitle from '../../common/PageTitle/PageTitle';
+import SideImage from '../../common/SideImage/SideImage';
+import DetailsBox from '../../common/DetailsBox/DetailsBox';
+import DetailsImage from '../../common/DetailsImage/DetailsImage';
+import List from '../../common/List/List';
+import ListItem from '../../common/ListItem/ListItem';
+import OrderForm from '../../features/OrderForm/OrderFormContainer';
 
+import styles from './Trip.scss';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import TripListOptions from '../../features/TripListOptions/TripListOptionsContainer';
 
-const Trips = ({ trips }) => (
-  <Section>
-    <Grid>
-      <Row>
-        <Col xs={12}>
-          <PageTitle text='All trips' />
-          <TripListOptions />
+const Trip = ({ error, name, image, cost, days, description, country, intro, id }) => {
+  if (error) return <NotFound />;
+  else return (
+    <Section>
+      <Grid>
+        <PageTitle text={name} />
+      </Grid>
+      <DetailsBox>
+        <DetailsImage>
+          <SideImage source={image} />
+        </DetailsImage>
+        <Grid>
           <Row>
-            {trips.length ? trips.map(trip => (
-              <TripSummary key={trip.id} {...trip} />
-            )) : (
-              <p>Sorry, no results found. Try adjusting the filters.</p>
-            )}
+            <Col md={12} lg={4}>
+              <div className={styles.intro}>
+                {HTMLParser(intro)}
+              </div>
+              <List variant='light'>
+                <ListItem title={`<strong>Duration:</strong> ${days} days`} icon='calendar-alt' />
+                <ListItem title={`<strong>Price:</strong> from ${cost}`} icon='money-bill-wave' />
+              </List>
+            </Col>
           </Row>
-        </Col>
-      </Row>
-    </Grid>
-  </Section>
-);
-
-Trips.propTypes = {
-  trips: PropTypes.arrayOf(PropTypes.object),
+        </Grid>
+      </DetailsBox>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <PageTitle text='Trip details' />
+            {HTMLParser(description)}
+          </Col>
+        </Row>
+      </Grid>
+      <Grid>
+        <PageTitle text={`About ${country.name}`} />
+      </Grid>
+      <DetailsBox>
+        <DetailsImage>
+          <SideImage source={country.flag} />
+        </DetailsImage>
+        <Grid>
+          <Row>
+            <Col md={12} lg={4}>
+              <List variant='light'>
+                <ListItem title={`<strong>Capital:</strong> ${country.capital}`} icon='city' />
+                <ListItem title={`<strong>Population:</strong> ${country.population / 1000000} millions`} icon='users' />
+                <ListItem title={`<strong>Currency:</strong> ${country.currencies[0].symbol} (${country.currencies[0].name})`} icon='money-bill-wave' />
+              </List>
+            </Col>
+          </Row>
+        </Grid>
+      </DetailsBox>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <PageTitle text='Trip options' />
+            <OrderForm tripCost={cost} tripId={id} tripName={name} tripCode={country.alpha3Code} />
+          </Col>
+        </Row>
+      </Grid>
+    </Section>
+  );
 };
 
-export default Trips;
+Trip.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  cost: PropTypes.string,
+  days: PropTypes.number,
+  description: PropTypes.string,
+  country: PropTypes.object,
+  id: PropTypes.string,
+};
+
+export default Trip;
